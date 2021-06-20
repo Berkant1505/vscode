34ebd0c65a87a35b7b23afb6510eb743207e0408
Berkant <berkant.gor@gmail.com>
2021-06-20 18:38:07 +0300

    vscode

---
+++ PYTHON/051621.py
@@ -0,0 +1,69 @@
+# print("odd" if int(input("Enter a num: "))%2 else "even")
+
+
+# a,b,c=input("Enter three inputs ").split()
+
+# print(a, b, c)
+
+
+
+
+# elma = 'portakal'
+# uzum = 'kiraz'
+
+# elma, uzum = uzum, elma
+# print(uzum)
+
+
+
+# li=[1,5,8,6,5,9,6,9,5,6,9,6,5,4]
+# print(list(set(li)))
+
+
+
+# li=[1,5,8,6,5,9,6,9,5,6,9,6,5,4,"a","a","b","b","a","a","a"]
+# print(max(set(li), key=li.count))
+
+
+
+# li=[i**3 for i in range(1,15) if i%2==0]
+# print(li)
+
+
+# name = 'Fatih Tepe'
+# print(name[::-1])
+
+# import math
+# print(math.__doc__)
+
+
+
+# from flask import Flask
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#     return "hello world"
+
+
+# from rich import prin,
+
+# print('hello world')
+
+
+
+# @app.route('/time')
+# def index():
+#     now = datetime.now()
+#     current_time = now.strftime('%D %H:%M:%S')
+#     return {'time': current_time}
+
+# print(index())
+
+# mystring = 'DevOps'
+
+
+
+import logging
+
+
---
+++ PYTHON/import_os_clear.py
@@ -0,0 +1,18 @@
+import os 
+
+clear = lambda: os.system("clear")
+clear()
+
+# def say_hello():
+#     print('hello everyone!!!')
+    
+# print('this is the first print statement')
+# say_hello()
+
+
+
+import datetime
+
+# print(datetime.date.today)
+
+print(dir(datetime))
